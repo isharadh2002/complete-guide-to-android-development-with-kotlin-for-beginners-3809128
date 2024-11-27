32ae5c096ba3a70e5b83d7c61f3780bf7c9465c2
Ishara Dhanushan <isharadh2002@gmail.com>
2024-11-28 00:03:41 +0530

    Doing exercises

--- app/src/test/java/com/example/myspecial/application/Expressions.kt
+++ app/src/test/java/com/example/myspecial/application/Expressions.kt
@@ -24,18 +24,23 @@ class ExpressionsTest {
     @Test
     fun ifAsExpression() {
         // TODO: print the results of the isEven function with `2`
-
+        var value :Int = 2
+        println("Number $value is " + isEven(value))
 
         // TODO: print the results of the isEven function with `3`
-
+        value = 3
+        println("Number $value is " + isEven(value))
     }
 
     @Test
     fun tryCatchAsExpression() {
         // TODO: print the results of the stringToInt function with `"2"`
-
+        var value :String = "2"
+        println("String \"$value\" converted to int : " + stringToInt(value))
 
         // TODO: print the results of the stringToInt function with `"cake"`
+        value = "cake"
+        println("String \"$value\" converted to int : " + stringToInt(value))
 
     }
 
--- app/src/test/java/com/example/myspecial/application/Functions.kt
+++ app/src/test/java/com/example/myspecial/application/Functions.kt
@@ -3,25 +3,33 @@ package com.example.myspecial.application
 import org.junit.Test
 
 // TODO: Create the multiply function with `left` and `right`
-
+fun multiply(left: Int = 1, right: Int = 1): Int {
+    return left * right
+}
 
 class FunctionsTest {
 
     @Test
     fun functionCreation() {
         // TODO: print the results of the multiply function
-
+        var result = multiply(2, 3)
+        println("The result of multiplication function : $result")
 
         // TODO: print the results of the multiply function with named
+        result = multiply(left = 2, right = 3)
+        println("The result of multiplication function with named parameters : $result")
 
     }
 
     @Test
     fun functionDefaultValues() {
         // TODO: print the results of the multiply function with defaults
+        var result: Int = multiply()
+        println("The result of multiplication function with defaults : $result")
 
 
         // TODO: print the results of the multiply function with named defaults
-
+        result = multiply(left = 2)
+        println("The result of multiplication function with named defaults : $result")
     }
 }
