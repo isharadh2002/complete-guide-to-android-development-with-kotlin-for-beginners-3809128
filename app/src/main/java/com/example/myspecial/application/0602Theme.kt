60ea829d4d7a0caa5cbba9f1d459a5355a8ee318
Ishara Dhanushan <isharadh2002@gmail.com>
2024-11-28 21:31:34 +0530

    Doing exercises

--- app/src/main/java/com/example/myspecial/application/0602Theme.kt
+++ app/src/main/java/com/example/myspecial/application/0602Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Checkbox
@@ -81,6 +82,7 @@ fun ThemeComposable(modifier: Modifier = Modifier) {
         Spacer(Modifier.size(24.dp))
 
         FilledTonalButton(
+            shape = RoundedCornerShape(10.dp),
             modifier = Modifier
                 .padding(16.dp)
                 .fillMaxWidth(),
