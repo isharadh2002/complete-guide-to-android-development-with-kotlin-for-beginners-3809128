d6e2b42ada300c2fbd2cc99e6d6d5870fbc01500
Ishara Dhanushan <isharadh2002@gmail.com>
2024-11-29 00:33:09 +0530

    Doing changes to ScaffoldTrial.kt

--- app/src/main/java/com/example/myspecial/application/ui/ScaffoldTrial.kt
+++ app/src/main/java/com/example/myspecial/application/ui/ScaffoldTrial.kt
@@ -3,6 +3,7 @@ package com.example.myspecial.application.ui
 import android.os.*
 import androidx.activity.*
 import androidx.activity.compose.*
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.*
 import androidx.compose.material.icons.filled.*
@@ -10,13 +11,13 @@ import androidx.compose.material3.*
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.*
+import androidx.compose.ui.text.font.*
 import androidx.compose.ui.text.style.*
 import androidx.compose.ui.tooling.preview.*
 import androidx.compose.ui.unit.*
 import com.example.myspecial.application.R
-import com.example.myspecial.application.ui.theme.AppTheme
-import com.example.myspecial.application.ui.theme.MySpecialApplicationTheme
+import com.example.myspecial.application.ui.theme.*
 
 class ScaffoldTrial : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -72,8 +73,10 @@ fun ScaffoldExample() {
                 .padding(innerPadding),
             verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
+
+            Greeting("Android!!!", Modifier)
             Text(
-                modifier = Modifier.padding(8.dp),
+                modifier = Modifier.padding(10.dp),
                 text =
                 """
                     This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
@@ -82,6 +85,9 @@ fun ScaffoldExample() {
 
                     You have pressed the floating action button $presses times.
                 """.trimIndent(),
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onBackground,
+                textAlign = TextAlign.Justify
             )
         }
     }
@@ -96,5 +102,38 @@ fun ScaffoldExample() {
 fun ScaffoldExamplePreview() {
     AppTheme {
         ScaffoldExample()
+    }
+}
+
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier){
+    Column (
+        modifier = modifier.fillMaxWidth()
+    ){
+        Text(
+            modifier = modifier.align(Alignment.CenterHorizontally)
+                .padding(top = 30.dp, bottom = 5.dp, start = 10.dp, end = 10.dp)
+                .fillMaxWidth()
+                .clickable {  },
+            text = "Hello $name",
+            style = MaterialTheme.typography.headlineLarge,
+            fontWeight = FontWeight.Light,
+            color = MaterialTheme.colorScheme.tertiary,
+            textAlign = TextAlign.Center
+        )
+
+    }
+}
+
+@Preview(
+    showBackground = true,
+    device = Devices.PIXEL_7_PRO
+)
+@Composable
+fun GreetingPreview() {
+    MySpecialApplicationTheme {
+        Greeting("Android", Modifier)
     }
 }
