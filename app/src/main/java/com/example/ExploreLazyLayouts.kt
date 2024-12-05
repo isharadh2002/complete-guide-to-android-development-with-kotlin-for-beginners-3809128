bfe13d3db63924a8e3c7fe015ddad17e73314906
Ishara Dhanushan <isharadh2002@gmail.com>
2024-12-05 20:11:52 +0530

    Doing exercises

--- app/src/main/java/com/example/ExploreLazyLayouts.kt
+++ app/src/main/java/com/example/ExploreLazyLayouts.kt
@@ -20,7 +20,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.two.trees.R
+import com.example.myspecial.application.R
 import com.example.two.trees.ui.theme.AppTheme
 
 // LazyColumn example
