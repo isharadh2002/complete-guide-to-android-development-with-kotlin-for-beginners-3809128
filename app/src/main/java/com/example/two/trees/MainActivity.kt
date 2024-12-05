9b765d6a3507810ad5f39cc57426aee11c8cd7eb
Ishara Dhanushan <isharadh2002@gmail.com>
2024-12-05 21:02:29 +0530

    Doing exercises

--- app/src/main/java/com/example/two/trees/MainActivity.kt
+++ app/src/main/java/com/example/two/trees/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
@@ -19,10 +23,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myspecial.application.R
 import com.example.two.trees.ui.theme.AppTheme
 
@@ -39,7 +46,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TwoTreesApp() {
     var checked by remember { mutableStateOf(true) }
-    val imageId = if (checked) R.drawable.medium_olive_oil else R.drawable.olive_branch
+    val imageId = if (checked) R.drawable.medium_olive_oil else R.drawable.olive_branch_vector
 
     AppTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -59,7 +66,10 @@ fun TwoTreesApp() {
 
                 Image(
                     painter = painterResource(imageId),
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier.size(256.dp)
+                        .clip(RoundedCornerShape(25.dp))
+                        .background(Color.Green)
+                        .padding(15.dp),
                     contentScale = ContentScale.Fit,
                     contentDescription = null
                 )
