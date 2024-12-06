7922083b3f4f6c4a89b1148b46353f446d06b914
Ishara Dhanushan <isharadh2002@gmail.com>
2024-12-06 21:07:42 +0530

    Doing exercises

--- app/src/main/java/com/example/two/trees/MainActivity.kt
+++ app/src/main/java/com/example/two/trees/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -27,6 +29,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -82,7 +86,10 @@ fun TwoTreesApp() {
 
                 Image(
                     painter = painterResource(imageId),
-                    modifier = Modifier.size(256.dp),
+                    modifier = Modifier.size(256.dp)
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(color = MaterialTheme.colorScheme.primaryContainer)
+                        .padding(10.dp),
                     contentScale = ContentScale.Fit,
                     contentDescription = null,
                 )
