3e1c4efecfa750a6bf2d2ffa028a3a751ba8dcfd
Ishara Dhanushan <isharadh2002@gmail.com>
2024-12-07 11:29:16 +0530

    Doing exercises

--- app/src/main/java/com/example/two/trees/MainActivity.kt
+++ app/src/main/java/com/example/two/trees/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,16 +13,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
@@ -50,29 +55,62 @@ fun TwoTreesApp() {
                 TwoTreesAppBar()
             }
         ) { innerPadding ->
-            ImageSwapper(
-                modifier = Modifier.padding(innerPadding),
-            )
+            val imageID: MutableIntState = remember { mutableIntStateOf(R.drawable.olive_branch_vector) }
+            val imageID2: MutableIntState = remember { mutableIntStateOf(R.drawable.logo) }
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                ImageSwapper(
+                    modifier = Modifier.padding(innerPadding),
+                    imageID = imageID.intValue,
+                    swapFunction = {
+                        if (imageID.intValue == R.drawable.olive_branch_vector) {
+                            imageID.intValue = R.drawable.logo
+                        } else {
+                            imageID.intValue = R.drawable.olive_branch_vector
+                        }
+                    }
+                )
+
+                ImageSwapper(
+                    modifier = Modifier.padding(innerPadding),
+                    imageID = imageID2.intValue,
+                    swapFunction = {
+                        if (imageID2.intValue == R.drawable.olive_branch_vector) {
+                            imageID2.intValue = R.drawable.logo
+                        } else {
+                            imageID2.intValue = R.drawable.olive_branch_vector
+                        }
+                    }
+                )
+            }
         }
     }
 }
 
 @Composable
 fun ImageSwapper(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    imageID: Int,
+    swapFunction: () -> Unit
 ) {
     Column(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        val imageId: MutableState<Int> = remember {
-            mutableIntStateOf(R.drawable.olive_branch_vector)
-        }
+
 
         Image(
-            painter = painterResource(imageId.value),
-            modifier = Modifier.fillMaxWidth(),
+            painter = painterResource(imageID),
+            modifier = Modifier
+                .size(256.dp)
+                .clip(RoundedCornerShape(20.dp))
+                .background(color = MaterialTheme.colorScheme.primaryContainer)
+                .padding(10.dp),
             contentScale = ContentScale.Fit,
             contentDescription = null
         )
@@ -81,10 +119,7 @@ fun ImageSwapper(
 
         Button(
             onClick = {
-                imageId.value = if (imageId.value == R.drawable.olive_branch_vector)
-                    R.drawable.logo
-                else
-                    R.drawable.olive_branch_vector
+                swapFunction()
             }
         ) {
             Text(
