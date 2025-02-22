be6766267a0b2567c4ec8fe5414e64937c8423de
Ishara Dhanushan <109136246+isharadh2002@users.noreply.github.com>
2025-02-22 23:11:47 +0530

    Doing exercises

--- app/src/main/java/com/example/two/trees/MainViewModel.kt
+++ app/src/main/java/com/example/two/trees/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.example.two.trees
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.two.trees.data.Product
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
@@ -13,7 +14,15 @@ class MainViewModel : ViewModel() {
     val quantity: StateFlow<Int> = _quantity
 
     init {
+        val product = Product(
+            productName = "Tomato Sauce",
+            imageFile = "tomato_sauce.jpg",
+            description = "Fresh tomatoes in a delicious sauce",
+            size = 10,
+            price = 1.99
+        )
         Log.i(TAG, "initialized")
+        Log.i(TAG, product.toString())
     }
 
 }
---
+++ app/src/main/java/com/example/two/trees/data/Product.kt
@@ -0,0 +1,9 @@
+package com.example.two.trees.data
+
+data class Product (
+    val productName: String,
+    val imageFile: String,
+    val description: String,
+    val size: Int,
+    val price: Double
+)
